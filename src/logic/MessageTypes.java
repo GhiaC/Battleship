351965251ec6acefa8422159ab956d2604a34fa1
ghiac <ghiac@aut.ac.ir>
2017-07-05 17:47:59 +0430

    add three class of logic

---
+++ src/logic/MessageTypes.java
@@ -0,0 +1,13 @@
+package logic;
+
+public class MessageTypes {
+    /**
+     * Version of communication protocol
+     */
+    public static final byte PROTOCOL_VERISON =1;
+
+    /**
+     * Code of request login message
+     */
+    public static final byte REQUEST_LOGIN =1;
+}
